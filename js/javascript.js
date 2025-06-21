0c5139cfa0d4f76dbdb840a2ed4c51261a40ff02
Ahmedelneel2 <ahmedalaaelneel3@gmail.com>
2025-06-21 12:26:42 +0300

    no bookmark with same name

--- js/javascript.js
+++ js/javascript.js
@@ -1,51 +1,59 @@
 var bookMarkName = document.getElementById("name")
 var url = document.getElementById("url")
+var index;
   if(localStorage.getItem("localAll") != null){
        var allUrl = JSON.parse(localStorage.getItem("localAll"))
        display()
     }else{
         var allUrl = []
     }
-// var Aurl = [{name:"google",url:"www.google.com"}]
-// console.log(Aurl.json.stringify())
-// localStorage.setItem("allUrl" , )
+
+function chicklisted(){
+   for(var i =0 ; i< allUrl.length ; i++){
+    if(bookMarkName.value == allUrl[i].name)
+    return false
+   }
+   return true 
+}
 function addBookMark(){
-     var urlRegexp =/^www.[a-z]+.com$/
+   if(chicklisted() == true){
+      var urlRegexp =/^www.[a-z]+.com$/
     var nameRegexp = /^[a-zA-Z]{3,}/
+   
     if(urlRegexp.test(url.value) && nameRegexp.test( bookMarkName.vlue)){
-        var Bookmark= {
-    name:bookMarkName.value,
-    url:url.value,
-  }
-  allUrl.push(Bookmark)
-  localStorage.setItem("localAll" , JSON.stringify(allUrl))
-  clearInput()
-  display()
-   document.getElementById("warning").classList.replace("d-block" ,"d-none" )
+        
+            var Bookmark= {
+        name:bookMarkName.value,
+        url:url.value,
+    }
+    allUrl.push(Bookmark)
+    localStorage.setItem("localAll" , JSON.stringify(allUrl))
+    clearInput()
+    display()
+    document.getElementById("warning").classList.replace("d-block" ,"d-none" )
     }else{
         document.getElementById("warning").classList.replace("d-none" ,"d-block")
     }
-//   if(search() == true){
-     
-//   }else{
-//     console.log("there is a Bookmark with the same name")
-//   }
+
+
   
+}else{
+    Swal.fire({
+  icon: "error",
+  title: "Oops...",
+  text: "there is a book mark with the same name ",
+});
+    
 }
-// function search(){
-//   var  BookmarkS  =bookMarkName.value.trim().toLowerCase()
-//     for(var i =0 ; i< allUrl.length ; i++){
-//         if(allUrl[i].name.toLowerCase() != BookmarkS){
-//            console.log("yes")
-//         }else{
-//            console.log("no")
-//         }
-//     }
-// }
+}
+
+
 function clearInput(){
     bookMarkName.value =""
     url.value = ""
-}
+   }
+
+
 function display(){
     cartona =""
     for(var i=0 ;i<allUrl.length ;i++){
